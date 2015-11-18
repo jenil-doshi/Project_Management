09b88e9141b160a29a01f864dc0eda3dfb2f5057
jenil-doshi <jenil.doshi@sjsu.edu>
2015-11-18 02:27:43 -0800

    First Committ

---
+++ CMPE275_TermProject/src/test/java/com/sjsu/cmpe275/projectmanager/test/SampleTest.java
@@ -0,0 +1,5 @@
+package com.sjsu.cmpe275.projectmanager.test;
+
+public class SampleTest {
+
+}
---
+++ CMPE275_TermProject/src/test/resources/com/sjsu/cmpe275/projectmanager/resource/SampleResourceTest.java
@@ -0,0 +1,5 @@
+package com.sjsu.cmpe275.projectmanager.resource;
+
+public class SampleResourceTest {
+
+}
