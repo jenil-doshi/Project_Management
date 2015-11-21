6fb2c798f7c1070f9a01fd52ab0ddfafafe73e8b
krutipandya <kruti.pandya90@gmail.com>
2015-11-20 19:03:37 -0800

    Adding config files for Web Security

--- CMPE275_TermProject/src/main/java/com/sjsu/cmpe275/projectmanager/configuration/AppConfig.java
+++ CMPE275_TermProject/src/main/java/com/sjsu/cmpe275/projectmanager/configuration/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -14,6 +15,14 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = "com.sjsu.cmpe275.projectmanager")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
